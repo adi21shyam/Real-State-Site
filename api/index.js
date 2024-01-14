69787c8a7110a4f3986be64ad247fe850ea78072
Aditya Singh <aditya.singh@criodo.com>
2024-01-14 11:59:58 +0530

    Project Completed

--- api/index.js
+++ api/index.js
@@ -3,17 +3,28 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import listingRouter from "./routes/listing.route.js"
+import path from "path"
+
 dotenv.config();
 import cors from "cors"
+import cookieParser from 'cookie-parser';
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MongoDB!");
 }).catch(err=>{
+    console.log("123")
     console.log(err);
 });
 
+
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
+
+
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!!');
@@ -27,6 +38,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"client", "dist", "index.html"));
+})
 
 
 app.use((err, req,res,next)=>{
